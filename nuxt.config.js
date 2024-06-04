3883f683662da914c0a217f43e13f97cb1fb82a8
長谷川泰斗 <hasegawataito@hasegawataitonoMacBook-Pro.local>
2024-06-04 17:24:05 +0900

    firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,13 +102,17 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['nuxt-microcms-module'],
+  buildModules: ['nuxt-microcms-module', '@nuxtjs/google-gtag'],
   microcms: {
     options: {
       serviceDomain: process.env.SERVICE_DOMAIN,
       apiKey: process.env.API_KEY,
     },
     mode: process.env.NODE_ENV === 'production' ? 'server' : 'all',
+  },
+  'google-gtag': {
+    id: 'G-EX0Q3V9TLM',
+    debug: true
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
